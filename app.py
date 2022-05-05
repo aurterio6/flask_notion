ba599f2dd7fe7efbd190395afbbede882e102d5e
aurterio6 <77596337+aurterio6@users.noreply.github.com>
2022-05-05 23:34:25 +0900

    add favicon

--- app.py
+++ app.py
@@ -288,7 +288,8 @@ def tagpage(Tag):
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 @app.route('/sitemap.txt')
-@app.route('/default.jpeg')
+@app.route('/default.jpg')
+@app.route('/favicon.ico')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
